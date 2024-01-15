5f6d2412c69dc9002df91d50aba3b8f527e3e4be
Allie c( ⁰ 〰 ⁰ )੭ <azhang@users.noreply.github.com>
2024-01-15 01:47:35 -0800

    allow selecting country code from .env

--- build/get_wireguard_config.sh
+++ build/get_wireguard_config.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 # Clear the output folder
-rm -f -r /output/*
+#rm -f -r /output/*
+#
+#example: curl 'https://api.nordvpn.com/v1/servers/recommendations?filters\[country_id\]=2&\[servers_technologies\]\[identifier\]=wireguard_udp&limit=1"'    
+
+api_url="https://api.nordvpn.com/v1/servers/recommendations?filters"
+technology_filter="\[servers_technologies\]\[identifier\]=wireguard_udp"
+
+# Get the country code from the country name (if provided) and add it to the API URL as a filter
+echo $COUNTRY_CODE
+if [[ -n ${COUNTRY_CODE} ]]; then
+  country_id=$(curl --silent "https://api.nordvpn.com/v1/servers/countries" | jq --raw-output ".[] | select(.code == \"${COUNTRY_CODE}\") | [.name, .id] | \"\(.[1])\"")
+  echo "[$(date -Iseconds)] Country ID: ${country_id}"
+  country_filter="\[country_id\]=${country_id}"
+  api_url="${api_url}${country_filter}&${technology_filter}"
+  echo "[$(date -Iseconds)] API URL: ${api_url}"
+else
+  api_url="${api_url}${technology_filter}"
+fi
 
 # Get the chosen server
-api_response=$(curl -s ${API_QUERY:-"https://api.nordvpn.com/v1/servers/recommendations?&filters\[servers_technologies\]\[identifier\]=wireguard_udp&limit=1"})
+api_response=$(curl --retry 3 -LsS "${api_url}&${QUERY}&limit=1")
+echo "${api_url}&${QUERY}&limit=1"
 server_identifier=$(jq -r '.[]|.hostname' <<< "$api_response" | cut -d "." -f 1)
 server_hostname=$(jq -r '.[]|.hostname' <<< "$api_response")
 server_ip=$(jq -r '.[]|.station' <<< "$api_response")
